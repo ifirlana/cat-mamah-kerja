3e8a858f6390787a3c0ff3a3bfac88067ddb383c
ikhlas firlana <ifirlana@gmail.com>
2017-06-16 00:23:27 +0700

    no message

---
+++ my/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+/**
+ * Created by ikhlasfirlana on 6/15/17.
+ */
+@Component({
+  selector:'app-server',
+  templateUrl:'./server.component.html'
+})
+export class ServerComponent {
+
+}
